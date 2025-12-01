9c2cd46dbe13ea9f75e9e1755362112cff299b81
DayVee-Programming <davronbek4business@gmail.com>
2025-12-01 20:11:06 +0500

    feat(packages): add '@vercel/analytics' package

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./sass/main.scss";
 import "./i18n/index.js";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <React.Suspense fallback={<p>Loading...</p>}>
       <App />
+      <Analytics/>
     </React.Suspense>
   </React.StrictMode>
 );
